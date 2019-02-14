10997d4c8d0ff4da1f0c9011795272f8321d4485
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-02-14 21:32:17 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate rand;
+use rand::prelude::*;
+ 
+fn main() {
+    println!("Hello, world!");
+}
+
+
+    // let seed = [6,4,3,8, 7,9,8,10, 14,18,12,12, 14,15,16,17];
+    // let mut rng = SmallRng::from_seed(seed);
+    //         let r = rng.gen_range(min_per_layer, max_per_layer + 1);
